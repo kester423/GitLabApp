2a43ea2ad27fd68ca3b995d338a06d2f20b7acf0
kester423 <kester11012006@gmail.com>
2025-03-10 16:02:44 +0300

    Первый коммит

--- GitLabApp/Program.cs
+++ GitLabApp/Program.cs
@@ -1,4 +1,4 @@
-﻿0using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,12 @@ namespace GitLabApp
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            double x = double.Parse(s);
+            double F = 2 * Math.Cos(x);
+            Console.WriteLine(x + " " + F);
+            Console.ReadKey();
+            return;
         }
     }
 }

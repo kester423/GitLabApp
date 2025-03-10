a10e42b11514a29ea966f8746a81d0ca3572addc
kester423 <kester11012006@gmail.com>
2025-03-10 15:33:03 +0300

    Добавьте файлы проекта.

---
+++ GitLabApp/Program.cs
@@ -0,0 +1,15 @@
+﻿0using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

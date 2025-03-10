b7a18d6c610332a4ce0cdcbd4722452d63cb0957
kester423 <kester11012006@gmail.com>
2025-03-10 16:44:35 +0300

    Втоой коммит

--- GitLabApp/Program.cs
+++ GitLabApp/Program.cs
@@ -10,12 +10,12 @@ namespace GitLabApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значение параметра x: ");
             string s = Console.ReadLine();
-            double x = double.Parse(s);
+            double x = Convert.ToDouble(s);
             double F = 2 * Math.Cos(x);
-            Console.WriteLine(x + " " + F);
+            Console.WriteLine($"Значение F = {F}.");
             Console.ReadKey();
-            return;
         }
     }
 }
